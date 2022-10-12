e910ad65bf5344d9350e87b03e8cc3a398227722
Leonel Hinojosa Maldonado <emilianomaldonado675@gmail.com>
2022-10-12 19:30:02 -0300

    entregafinal

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT||8080;
+const router = express.Router();
+const { engine } = require("express-handlebars");
+const productRouter = require("./routes/products")(router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/productos", productRouter);
+app.set("views", "./src/views");
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+
+app.get("/*", (req, res) => {
+  res.render("pages/home", {});
+});
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+server.on("error", (err) => console.log(err));
---
+++ src/routes/products.js
@@ -0,0 +1,52 @@
+module.exports = function (router) {
+    const bodyParser = require("body-parser");
+    const Contenedor = require("../contenedor.js");
+    const nuevo = new Contenedor("./productos.txt");
+    let urlencodedParser = bodyParser.urlencoded({ extended: false });
+  
+    router.get("/", async (req, res) => {
+      const productos = await nuevo.getAll();
+      res.render("pages/list", { productos });
+    });
+  
+    router.get("/:id", async (req, res) => {
+      const producto = await nuevo.getById(req.params.id);
+      producto
+        ? res.status(200).json(producto)
+        : res
+            .status(400)
+            .json({ error: "Ocurrió un error al encontrar el producto." });
+    });
+  
+    router.get("/crear", async (req, res) => {
+      res.render("pages/form", {});
+    });
+  
+    router.post("/", urlencodedParser, async (req, res) => {
+      const {body} = req;
+      body.timestamp = Date.now();
+      await nuevo.save(body);
+      res.redirect("/productos/crear");
+    });
+  
+    router.put("/:id", async (req, res) => {
+      const productUpdated = await nuevo.updateById(req.params.id - 1, req.body);
+      productUpdated
+        ? res.status(200).send({ success: "Producto actualizado." })
+        : res
+            .status(404)
+            .send({ error: "Ocurrió un error al encontrar el producto." });
+    });
+  
+    router.delete("/:id", async (req, res) => {
+      const productDeleted = await nuevo.deleteById(Number(req.params.id));
+      productDeleted
+        ? res.status(200).send({ success: "Producto eliminado." })
+        : res
+            .status(404)
+            .send({ error: "Ocurrió un error al encontrar el producto." });
+    });
+  
+    return router;
+  };
+  
